fbb65d9cbb51a16bb31cbe1e572b538a29ff2451
Daniel Benedek <benedekdj@cardiff.ac.uk>
2019-03-26 12:15:58 +0000

    Delete datepicker as other API needed atm

--- app/src/main/java/com/darekeapp/activities/HomeActivity.java
+++ app/src/main/java/com/darekeapp/activities/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.darekeapp.activities;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,7 +11,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.darekeapp.R;
@@ -25,7 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, DatePickerDialog.OnDateSetListener {
+        implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
 
@@ -121,8 +119,4 @@ public class HomeActivity extends AppCompatActivity
         navUserEmail.setText(currentUser.getEmail());
     }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
-    }
 }
